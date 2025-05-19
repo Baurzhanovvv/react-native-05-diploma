124cc21f31ee44ed39d8d27fe9830d76bb37c660
Baurzhanovvv <amirbaurzhanov@gmail.com>
2025-05-20 01:21:30 +0500

    stream chat

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,5 +1,46 @@
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
+import { useEffect } from "react";
+import { StreamChat } from 'stream-chat'
+import { Chat, OverlayProvider } from "stream-chat-expo";
+
+const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_API_KEY!);
 
 export default function HomeLayout() {
-    return <Stack />
+    
+    useEffect(() => {
+        const connect = async () => {
+          try {
+            await client.connectUser(
+              {
+                id: "jlahey",
+                name: "Jim Lahey",
+                image: "https://i.imgur.com/fR9Jz14.png",
+              },
+              client.devToken("jlahey")
+            );
+      
+            // const channel = client.channel("messaging", "the_park", {
+            //   name: "The Park",
+            // });
+            // await channel.watch();
+            console.log("User connected and channel created");
+          } catch (err) {
+            console.error("Stream Chat connection error:", err);
+          }
+        };
+      
+        connect();
+      }, []);
+      
+
+    return (
+        <OverlayProvider>
+            <Chat client={client}>
+                <Stack>
+                    <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+                </Stack>
+            </Chat>
+        </OverlayProvider>
+    )
+        
 }
