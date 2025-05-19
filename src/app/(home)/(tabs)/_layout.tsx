5d2072cae9046c36bfa7761362add8a541135f4b
Baurzhanovvv <amirbaurzhanov@gmail.com>
2025-05-20 00:21:07 +0500

    expo router

---
+++ src/app/(home)/(tabs)/_layout.tsx
@@ -0,0 +1,21 @@
+import { Tabs } from "expo-router";
+import { FontAwesome5 } from '@expo/vector-icons'
+
+export default function TabsNavigator() {
+    return <Tabs>
+        <Tabs.Screen name='index' options={{
+            title: 'Chats',
+            tabBarIcon: ({color, size}) => (
+                <FontAwesome5 name="home" size={size} color={color} />
+            )
+        }} />
+        <Tabs.Screen name='profile' options={{
+            title: 'Profile',
+            tabBarIcon: ({color, size}) => (
+                <FontAwesome5 name="user-alt" size={size} color={color} />
+            )
+        }} 
+            
+        />
+    </Tabs>
+}
